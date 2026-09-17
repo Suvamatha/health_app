0674515fec1279c8dfd15bd1cd1c7bfdbf78a941
Suvamatha <shresthasuvam27@gmail.com>
2026-09-17 10:14:57 +0545

    making splash screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+  with SingleTickerProviderStateMixin {
+    late final AnimationController _controller;
+    late final Animation<double> _fadeAnimation;
+    late final Animation<double> _scaleAnimation;
+
+  @override 
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.0,0.6, curve: Curves.easeOut),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.85, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
+    );
+
+    _controller.forward();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
